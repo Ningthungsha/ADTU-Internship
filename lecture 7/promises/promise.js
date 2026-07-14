27fb960f1967964760d7f1b5238c59d58f284467
unknown <ningthungsharoel@gmail.com>
2026-07-14 10:46:15 +0530

    Added lecture 7

---
+++ lecture 7/promises/promise.js
@@ -0,0 +1,33 @@
+console.log('hello');
+
+// const promiseOne = new Promise((res,rej)=>{
+//     setTimeout(()=>{
+//         console.log("coding blocks");
+
+//         res();
+//     }, 5000)
+// })
+
+// promiseOne.then(()=>{
+//     console.log('completed');
+// })
+
+
+const promiseTwo = new Promise((res,rej)=>{
+    setTimeout(()=>{
+        let error = true;
+
+        if(!error){
+            res({name:'xyz', password: '123'});
+        }
+        else{
+            rej('ERROR: something went wrong');
+        }
+    },5000)
+})
+
+promiseTwo.then((user)=>{
+    console.log(user);
+}).catch((error)=>{
+    console.log(error);
+})
---
+++ lecture 7/promises/promissechaining.js
@@ -0,0 +1,21 @@
+let p = new Promise((res,rej)=>{
+    res();
+})
+
+p 
+    .then(()=>{
+        console.log("resolved");
+        return 2; //promise always return a new promise 
+    })
+    .then((data)=>{
+        console.log('prev promise return data');
+
+        return data*2;
+    })
+    .then((data)=>{
+        console.log(data);
+
+    })
+    .catch(()=>{
+        console.log('promise rejected');
+    })
